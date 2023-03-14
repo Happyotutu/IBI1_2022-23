b6c3e668eec205f603220e15ee94fca0d0ab5d14
Jiang Hangyuan <hangyuan.22@intl.zju.edu.cn>
2023-03-14 21:16:24 +0800

    W is False. Z is True.

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,6 +3,17 @@ b = -118.24
 c = 116.39
 d = a - b
 e = c - a
-print(d)
-print(e)
+If d > e
+ print("d")
+else 
+ print("e")
+
+#Booleans
+X = True
+Y = False
+W = X and Y
+Z = X or Y
+print(W)
+print(Z)
+
 
