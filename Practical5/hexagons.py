2f50ca826615d4b1df97158c8559ce626ee99aa7
Jiang Hangyuan <hangyuan.22@intl.zju.edu.cn>
2023-03-16 21:59:41 +0800

    Hexagonal sequence

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,7 +1,9 @@
+#When n = 1, 2, 3, 4, 5, calculate the value of n*(2n-1)
+#show the final values
+
 #make h and n are number variables
 h=0
 n=0
-# for first five values
 for n in range(1,6)
    h = n*(2*n-1) #the fomula
    print(h)     #to display the value
