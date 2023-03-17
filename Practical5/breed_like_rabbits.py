88f406839cd9d53bde48ea528cb582857279d615
Jiang Hangyuan <hangyuan.22@intl.zju.edu.cn>
2023-03-17 17:03:04 +0800

    At 7th generation

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -9,10 +9,13 @@
 #        Once more than 100, stop
 #    ! Stop when less than 100, so the generation should plus 1 to exceed 100
 
-sum=2
+sum=2 #initial number of rabbits
 #stop when <100, so next generation will >100
-n=1
+n=1 # generation number
 while sum<100:
- sum=sum*2
+ sum=sum*2 #the number of next generation = 2*the number of this generation
  n+=1
 print("At", str(n), "generation over 100 rabbits have been born.")
+
+# At 7 generation over 100 rabbits have been born.
+
