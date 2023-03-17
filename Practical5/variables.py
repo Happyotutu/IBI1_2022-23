158c108a648a5e1f3291ae121ddfea60c4921263
Jiang Hangyuan <hangyuan.22@intl.zju.edu.cn>
2023-03-17 17:05:09 +0800

    Rob travelled further to Haining. W is False. Z is true.

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,17 +3,22 @@ b = -118.24
 c = 116.39
 d = a - b
 e = c - a
-If d > e
- print("d")
-else 
- print("e")
+if d > e:
+ print("d=", d, "> e=", e)
+ print("Rob travelled further to Los Angeles.")
+else:
+ print("e=", e, "> d=", d)
+ print("Rob travelled further to Haining.")
+#Rob travelled further to Haining.
 
 #Booleans
 X = True
 Y = False
 W = X and Y
 Z = X or Y
-print(W)
-print(Z)
+print("W =", W )
+print("Z =", Z )
 
+# W = False
+# Z = True
 
