fa2f411d0e3b2d6d169845932eeeb648498972e5
Jiang Hangyuan <hangyuan.22@intl.zju.edu.cn>
2023-03-17 17:03:35 +0800

    hexagons sequence

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -2,10 +2,16 @@
 #show the final values
 
 #make h and n are number variables
-h=0
-n=0
+h=0 # the hexagons squence value
+n=0 # the hexagon number
 for n in range(1,6)
    h = n*(2*n-1) #the fomula
    print(h)     #to display the value
 
+#output:
+#1
+#6
+#15
+#28
+#45
 
