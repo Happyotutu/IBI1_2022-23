0554390f73543ae6dc48f69e8b3a2e06d6aff31a
Jiang Hangyuan <hangyuan.22@intl.zju.edu.cn>
2023-04-19 16:04:11 +0800

    add example

--- Practical10/triathlon.py
+++ Practical10/triathlon.py
@@ -7,9 +7,10 @@ class triathlon:
         self.f = first_name
         self.l = last_name
         self.lo = location
+        self.total = swim + cycle + run
     def out_put(self):
-        print("Name:", self.f, self.l," Location:", self.lo, " Time for swim:", self.s," Time for cycle:", self.c, " Time for run:", self.r," Total time over the triathlon:", self.c + self.r + self.s)
+        print("Name:", self.f, self.l," Location:", self.lo, " Time for swim:", self.s," Time for cycle:", self.c, " Time for run:", self.r," Total time over the triathlon:", self.total)
 #an example
 member1 = triathlon('Tutu', 'Jiang', 'Beijing', 100, 100, 200)
 member1.out_put()
-
+#Name: Tutu Jiang  Location: Beijing  Time for swim: 100  Time for cycle: 100  Time for run: 200  Total time over the triathlon: 400
