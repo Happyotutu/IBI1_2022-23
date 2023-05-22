4c51026446f0396df5ce9d3ef1866a710b8382a3
Jiang Hangyuan <hangyuan.22@intl.zju.edu.cn>
2023-05-22 19:02:14 +0800

    don't count TGA, count ATG

--- Practical10/seq_calculator.py
+++ Practical10/seq_calculator.py
@@ -1,3 +1,5 @@
+# the length of enconding sequence contains ATG, didn't contain TGA
+
 def DNA_cal(self):
     """
     When meet ATG, start counting. 
@@ -11,9 +13,8 @@ def DNA_cal(self):
     sequence = self.upper()
     start_seq = sequence[sequence.index("ATG"):]
     coding_seq = start_seq[:start_seq.index("TGA")]
-    len_code_seq = len(coding_seq)-3
-    print("The length of coding line is", len_code_seq)
-    per = len_code_seq/len(self)
+    print("The length of coding line is", len(coding_seq))
+    per = len(coding_seq)/len(self)
     percent = '{:.0%}'.format(per)
     print("The percent of coding sequence is", percent)
     return per
@@ -27,7 +28,7 @@ elif per < 0.1:
 else:
     print("This sequence is an unclear sequence.")
 #my example
-#Please input a DNA sequence: AAAAAAAATTTTTTATGCCCCGGGGGAAATTTTTGAAAAAAA
-#The length of coding line is 16
-#The percent of coding sequence is 38%
+#Please input a DNA sequence: AAAAAAAATTTTTATGCCCCGGGGAAATTTTTGAAAAAAA
+#The length of coding line is 18
+#The percent of coding sequence is 45%
 #This sequence is an unclear sequence.
